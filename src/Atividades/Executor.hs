e0f708603f47e407f1ebb0fe9571bed3c54626b1
CalebeAF02 <calebe.2324@gmail.com>
2026-05-16 16:25:17 -0300

    feat(Atividades): Lista Media

--- src/Atividades/Executor.hs
+++ src/Atividades/Executor.hs
@@ -7,6 +7,7 @@ import Atividades.ListaDificil
 main :: IO ()
 main = do
     putStrLn "\n--- Testando Lista Facil ---"
+
     putStr "\t> questao_2 10 3 = "
     print (questao_2 10 3)
 
@@ -20,7 +21,15 @@ main = do
     print (questao_9 2026)
     
     putStrLn "\n\n--- Testando Lista Media ---"
-    -- print (questao_1 [1,1,1,1])
+
+    putStr "\t> questao_1 [1,1,1,1] = "
+    print (questao_1 [1,1,1,1])
+
+    putStr "\t> questao_5 5 = "
+    print (questao_5 5)
+
+    putStr "\t> questao_6 [1,1,1,1] [1,1,1,1] = "
+    print (questao_6 [1,1,1,1] [1,1,1,1])
     
     putStrLn "\n\n--- Testando Lista Dificil ---"
     -- print (questao_11 20)
--- src/Atividades/ListaMedia.hs
+++ src/Atividades/ListaMedia.hs
@@ -3,10 +3,29 @@ module Atividades.ListaMedia where
 -- LISTA MÉDIA (Peso 1,0)
 
 -- 1. Escreva uma função que receba uma lista de 4 elementos e verifique se todos os elementos são iguais. (1,0)
+questao_1 :: [Int] -> Bool
+questao_1 [a,b,c,d] = (a==b) && (b==c) && (c==d)
+questao_1 _ = False
 
 -- 5. Crie uma função que retorne a lista de todos os divisores de um dado número. (1,0)
+questao_5 :: Int-> [Int]
+questao_5 0 = []
+questao_5 n = [x | x <- [1..n], n `mod` x == 0]
 
 -- 6. Escreva uma função que verifique se duas listas possuem os mesmos elementos (a ordem não importa). (1,0)
+
+-- Função auxiliar manual (corrigindo os nomes das variáveis)
+verifica_em :: Int -> [Int] -> Bool
+verifica_em x [] = False
+verifica_em x (y:ys) = (x == y) || verifica_em x ys
+
+questao_6 :: [Int] -> [Int] -> Bool
+questao_6 [] [] = True        -- Se ambas acabaram juntas, são iguais
+questao_6 [] _  = False       -- Se a primeira acabou e a segunda não, são diferentes
+questao_6 (x:xs) lista2
+
+    | verifica_em x lista2 = questao_6 xs [y | y <- lista2, y /= x]
+    | otherwise            = False
 
 -- 7. Escreva uma função que retorne o número de vezes que um dado elemento aparece numa lista. (1,0)
 
